25ea38ae5be2a9f4dda90cc1973835cb84d0b89c
Une Nyide <une.nyide@gmail.com>
2025-04-09 22:00:28 +0200

    switch to sketch api

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@ use nannou::prelude::*;
 
 struct Model {}
 
-fn model(app: &App) -> Model {
-    Model {}
-}
+// fn model(app: &App) -> Model {
+//     Model {}
+// }
+//
+// fn update(_app: &App, _model: &mut Model, _update: Update) {}
+//
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
-
-fn view(app: &App, _model: &Model, frame: Frame) {
-    frame.clear(NAVAJOWHITE);
+    draw.background().color(NAVAJOWHITE);
+    draw.to_frame(app, &frame).unwrap();
 }
 
 fn main() {
-    nannou::app(model).update(update).simple_window(view).run();
+    nannou::sketch(view).run();
 }
